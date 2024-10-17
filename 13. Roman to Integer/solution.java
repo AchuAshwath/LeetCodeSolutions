af78209a8657e52af00cb48a5174232ca054d0ba
AchuAshwath <achuashwath88@gmail.com>
2024-10-16 23:16:19 -0700

    prettified roman to integer

--- 13. Roman to Integer/solution.java
+++ 13. Roman to Integer/solution.java
@@ -7,46 +7,40 @@ class Solution {
                     result += 1;
                     break;
                 case 'V':
-                    if(i >0 && s.charAt(i-1)== 'I'){
+                    if(i >0 && s.charAt(i-1)== 'I')
                         result += 3;
-                    }else{
+                    else
                         result += 5;
-                    }
                     break;
                 case 'X':
-                    if(i >0 && s.charAt(i-1)== 'I'){
+                    if(i >0 && s.charAt(i-1)== 'I')
                         result += 8;
-                    }else{
+                    else
                         result +=10;
-                    }
                     break;
                 case 'L':
-                    if(i >0 && s.charAt(i-1)== 'X'){
+                    if(i >0 && s.charAt(i-1)== 'X')
                         result += 30;
-                    }else{
+                    else
                     result += 50;
-                    }
                     break;
                 case 'C':
-                    if(i >0 && s.charAt(i-1)== 'X'){
+                    if(i >0 && s.charAt(i-1)== 'X')
                         result += 80;
-                    }else{
+                    else
                         result += 100;
-                    } 
                     break;
                 case 'D':
-                    if(i > 0 && s.charAt(i-1)== 'C'){
+                    if(i > 0 && s.charAt(i-1)== 'C')
                         result += 300;
-                    }else{
+                    else
                         result += 500;
-                    }
                     break;
                 case 'M':
-                    if(i > 0 && s.charAt(i-1)== 'C'){
+                    if(i > 0 && s.charAt(i-1)== 'C')
                         result += 800;
-                    }else{
+                    else
                         result += 1000;
-                    }
                     break;
             }
         }   
